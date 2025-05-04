2ef944b1f57d7aeca60fb0e701b36d192048fd80
Muhaib Shamsher <muhaibshamsher9999@gmail.com>
2025-05-04 16:35:39 +0500

    Navbar for user

--- client/src/components/user/UserHeader.jsx
+++ client/src/components/user/UserHeader.jsx
@@ -3,83 +3,93 @@ import SearchBox from './SearchBox';
 import { useNavigate, Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../../store/slices/auth.slice.js';
-// import { useLogoutMutation } from '../slices/usersApiSlice';
+import { clearCartItems } from '../../store/slices/cart.slice.js';
 
 import { Navbar, Nav, Container, Badge, NavDropdown } from 'react-bootstrap';
-import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { FaShoppingCart, FaUser, FaBug } from 'react-icons/fa';
 import { toast } from 'react-toastify';
 
 
 export default function Header() {
-   const { userInfo } = useSelector(state => state.auth);
-   const { cartItems } = useSelector(state => state.cart);
-   const dispatch = useDispatch();
-   const navigate = useNavigate();
+  const { userInfo } = useSelector(state => state.auth);
+  const { cartItems } = useSelector(state => state.cart);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-   const logoutHandler = async () => {
-      try {
-         const res = await fetch('http://localhost:5000/api/v1/users/logout', {
-            method: 'GET',
-            credentials: 'include'
-         })
+  const logoutHandler = async () => {
+    try {
+      const res = await fetch('http://localhost:5000/api/v1/users/logout', {
+        method: 'GET',
+        credentials: 'include'
+      });
 
-         if (!res.ok) {
-            throw new Error("Logout failed!")
-         }
-
-         dispatch(logout());
-         navigate('/login');
-         toast.success('Logout successful');
-      } catch (error) {
-         toast.error(error.message);
+      if (!res.ok) {
+        throw new Error("Logout failed!");
       }
-   };
 
-   return (
-      <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect className='fixed-top z-2 '>
-         <Container>
-            <Link to='/' className="text-decoration-none">
-               <Navbar.Brand>MERN Shop</Navbar.Brand>
-            </Link>
+      dispatch(logout());
+      dispatch(clearCartItems());
+      navigate('/login');
+      toast.success('Logged out successfully');
+    } catch (error) {
+      toast.error(`${error.message}`);
+    }
+  };
 
-            <Navbar.Toggle aria-controls='basic-navbar-nav' />
-            <Navbar.Collapse id='basic-navbar-nav'>
-               <Nav className='ms-auto m-2'>
-                  <SearchBox />
-                  <Nav.Link>
-                     <Link to='/cart' className="text-decoration-none text-white">
-                        <FaShoppingCart style={{ marginRight: '5px' }} />
-                        Cart
-                        {cartItems.length > 0 && (
-                           <Badge pill bg='warning' style={{ marginLeft: '5px' }} className='text-dark'>
-                              <strong>
-                                 {cartItems.reduce((acc, item) => acc + item.qty, 0)}
-                              </strong>
-                           </Badge>
-                        )}
-                     </Link>
-                  </Nav.Link>
+  return (
+    <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect className='fixed-top z-3 shadow'>
+      <Container>
+        <Link to='/' className="text-decoration-none">
+          <Navbar.Brand className="d-flex align-items-center gap-2 text-warning fw-bold fs-4">
+            <FaBug /> BuggyBuy
+          </Navbar.Brand>
+        </Link>
 
-                  {userInfo ? (
-                     <NavDropdown title={`Hello👋, ${userInfo.name}`} id='username'>
-                        <Link to='/profile' className="text-decoration-none text-white">
-                           <NavDropdown.Item>Profile</NavDropdown.Item>
-                        </Link>
-                        <NavDropdown.Item onClick={logoutHandler}>
-                           Logout
-                        </NavDropdown.Item>
-                     </NavDropdown>
-                  ) : (
-                     <Nav.Link>
-                        <Link to='/login' className="text-decoration-none text-white">
-                           <FaUser style={{ marginRight: '5px' }} />
-                           Sign In
-                        </Link>
-                     </Nav.Link>
-                  )}
-               </Nav>
-            </Navbar.Collapse>
-         </Container>
-      </Navbar>
-   );
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='ms-auto align-items-center gap-3'>
+            <SearchBox />
+
+            <Nav.Link
+              as={Link}
+              to='/cart'
+              className="text-decoration-none text-white d-flex align-items-center"
+            >
+              <FaShoppingCart className="me-1" />
+              Cart
+              {cartItems.length > 0 && (
+                <Badge pill bg='warning' className='text-dark ms-2'>
+                  <strong>{cartItems.reduce((acc, item) => acc + item.qty, 0)}</strong>
+                </Badge>
+              )}
+            </Nav.Link>
+
+            {userInfo ? (
+              <NavDropdown
+                title={<span className="text-warning">👋 Hello, {userInfo.name}</span>}
+                id='username'
+                className='text-white'
+              >
+                <Link to='/profile' className="text-decoration-none">
+                  <NavDropdown.Item>Profile</NavDropdown.Item>
+                </Link>
+                <NavDropdown.Item onClick={logoutHandler}>
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+            ) : (
+              <Nav.Link
+                as={Link}
+                to='/login'
+                className="text-decoration-none text-white d-flex align-items-center"
+              >
+                <FaUser className="me-1" />
+                Sign In
+              </Nav.Link>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }

955d68c50395aa14b32d9346a0b83d4474e79134
Muhaib Shamsher <muhaibshamsher9999@gmail.com>
2025-05-05 14:07:38 +0500

    Routing by adding user and admin pages

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -3,12 +3,14 @@ import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } 
 import AdminLayout from '../layouts/AdminLayout.jsx';
 import UserLayout from '../layouts/UserLayout.jsx';
 
+import { UserPrivateRoute, AdminRoute } from '../private_routes/PrivateRoutes.jsx'
+
 import AdminLogin from '../pages/admin/AdminLogin.jsx';
-import AdminProfile from '../pages/admin/AdminProfile.jsx';
 import Dashboard from '../pages/admin/Dashboard.jsx';
-import ProductForm from '../pages/admin/ProductForm.jsx';
 import ProductList from '../pages/admin/ProductList.jsx';
+import ProductForm from '../pages/admin/ProductForm.jsx';
 import UserList from '../pages/admin/UserList.jsx';
+import OrderList from '../pages/admin/OrderList.jsx';
 
 import Home from '../pages/user/Home.jsx';
 import Register from '../pages/user/Register.jsx';
@@ -16,6 +18,11 @@ import Login from '../pages/user/Login.jsx';
 import Profile from '../pages/user/Profile.jsx';
 import Product from '../pages/user/Product.jsx';
 import Cart from '../pages/user/Cart.jsx';
+import Shipping from '../pages/user/Shipping.jsx';
+import PlaceOrder from '../pages/user/PlaceOrder.jsx';
+import OrderDetails from '../pages/user/OrderDetails.jsx';
+
+import PageNotFound from '../pages/PageNotFound.jsx';
 
 
 const router = createBrowserRouter(
@@ -25,19 +32,31 @@ const router = createBrowserRouter(
         <Route index element={<Home />} />
         <Route path="register" element={<Register />} />
         <Route path="login" element={<Login />} />
-        <Route path="profile" element={<Profile />} />
         <Route path="product/:id" element={<Product />} />
         <Route path="cart" element={<Cart />} />
+
+        <Route element={<UserPrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="shipping" element={<Shipping />} />
+          <Route path="place-order" element={<PlaceOrder />} />
+          <Route path="order-details/:id" element={<OrderDetails />} />
+        </Route>
       </Route>
 
-      <Route path="/admin" element={<AdminLayout />}>
-        <Route index element={<AdminLogin />} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="profile" element={<AdminProfile />} />
-        <Route path="products" element={<ProductList />} />
-        <Route path="products/new" element={<ProductForm />} />
-        <Route path="users" element={<UserList />} />
+      <Route path="/admin" element={<AdminLogin />} />
+
+      <Route path="/admin" element={<AdminRoute />}>
+        <Route element={<AdminLayout />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="product-list" element={<ProductList />} />
+          <Route path="product/create" element={<ProductForm />} />
+          <Route path="product/update/:id" element={<ProductForm />} />
+          <Route path="user-list" element={<UserList />} />
+          <Route path="order-list" element={<OrderList />} />
+        </Route>
       </Route>
+
+      <Route path="*" element={<PageNotFound />} />
     </>
   )
 );
